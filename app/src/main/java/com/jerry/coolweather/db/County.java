67cb51b6c32dabbd676ed1c8ea112d4dc6767877
JerryWang <495388884@qq.com>
2018-03-14 18:14:42 +0800

    ### 更新日志 1.提交数据库实体类文件

---
+++ app/src/main/java/com/jerry/coolweather/db/County.java
@@ -0,0 +1,47 @@
+package com.jerry.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Jerry on 2018/3/14.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountName() {
+        return countName;
+    }
+
+    public void setCountName(String countName) {
+        this.countName = countName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
