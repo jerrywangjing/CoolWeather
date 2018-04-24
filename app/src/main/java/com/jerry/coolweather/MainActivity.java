4dcacb22d7b0f9581405d288e91e5c122b6e685f
JerryWang <495388884@qq.com>
2018-04-24 11:17:55 +0800

    ### 更新日志 1.删除未使用的import包

--- app/src/main/java/com/jerry/coolweather/MainActivity.java
+++ app/src/main/java/com/jerry/coolweather/MainActivity.java
@@ -1,9 +1,6 @@
 package com.jerry.coolweather;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
